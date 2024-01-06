34818eb92896318417a04f8ea1e3cb9ae55ec732
Reis Stanovci <stanovcireis@gmail.com>
2024-01-06 19:47:06 +0100

    Create validateMatrix.php

---
+++ validateMatrix.php
@@ -0,0 +1,41 @@
+<?php
+function isValidMinesweeperMtx($matrix) {
+    if (empty($matrix) || empty($matrix[0])) {
+        return false;
+    }
+
+    $cols = count($matrix[0]);
+
+    foreach ($matrix as $row) {
+        if (count($row) !== $cols) {
+            return false;
+        }
+
+        foreach ($row as $cell) {
+            if (!is_numeric($cell) || ($cell !== -1 && ($cell < 0 || $cell > 8))) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+// Matricat mund te jene te cfaredo dimenzioni 9x9 16x16 16x30
+// Example usage:
+// -1 do te thote mina
+$validGrid = [
+    [0, 1, -1],
+    [2, -1, 3],
+    [1, 3, -1]
+];
+
+$invalidGrid = [
+    [0, 1, -1],
+    [2, -1, 10],  // Invalid: 10 is not a valid number of adjacent mines
+    [1, 3, -1]
+];
+
+// Kthejn vlere booleane : TRUE OSE FALSE
+var_dump(isValidMinesweeperMtx($validGrid));   
+var_dump(isValidMinesweeperMtx($invalidGrid)); 
+?>
