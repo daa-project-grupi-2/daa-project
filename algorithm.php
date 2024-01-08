cdf3a27f2a80cba0c3114845aa65b5e2195b39f9
Lorena Mekaj <lorenamekaj@gmail.com>
2024-01-08 02:58:17 +0100

    E3T4/Feature/ Created the functionality that will return the solution for FrontEnd

--- algorithm.php
+++ algorithm.php
@@ -1,25 +1,21 @@
 <?php
-// Adjacent directions array
+
 $adjacents = [
-  [1, 1], [-1, -1], [0, 1], [0, -1],
-  [1, 0], [-1, 0], [-1, 1], [1, -1]
+    [1, 1], [-1, -1], [0, 1], [0, -1],
+    [1, 0], [-1, 0], [-1, 1], [1, -1]
 ];
 
-// Mode qe merret nga user nga console eshte thjesht per ceshtje testimi. Ne realitet zgjedhjen e tille e marrim nga frontend
-$mode = readline("Choose the algorithm with which the game will be solved! DFS or BFS!");
+$mode = 'DFS';
 
-
-
-//Funksioni rekursiv i eksplorimit eshte njejte i implementuar per te dy algoritmet. Vetem qasja ne rounde te minesweeper ndryshon, pra ajo e ben dallimin
-//Implementation of DFS
-function explore(&$mat, $row, $col){
+function explore(&$mat, $row, $col)
+{
     global $adjacents;
     $mineCounter = 0;
     foreach ($adjacents as $direction) {
         $row1 = $row + $direction[0];
         $col1 = $col + $direction[1];
         if ($row1 >= 0 && $col1 >= 0 && $row1 < count($mat)
-        && $col1 < count($mat[0]) && $mat[$row1][$col1] === 'M') {
+            && $col1 < count($mat[0]) && $mat[$row1][$col1] === 'M') {
             $mineCounter++;
         }
     }
@@ -32,28 +28,26 @@ function explore(&$mat, $row, $col){
         $row1 = $row + $direction[0];
         $col1 = $col + $direction[1];
         if ($row1 >= 0 && $col1 >= 0 && $row1 < count($mat)
-        && $col1 < count($mat[0]) && $mat[$row1][$col1] === 'E') {
+            && $col1 < count($mat[0]) && $mat[$row1][$col1] === 'E') {
             explore($mat, $row1, $col1);
         }
     }
 }
 
-
-// Implementation of minesweeper rounds
-function minesweeperRound($matrix){
+function minesweeperRound($matrix)
+{
     global $mode;
     $click = [];
-   
+
     $rows = count($matrix);
     $cols = count($matrix[0]);
 
-    if(strtoupper($mode)=='BFS'){
-        // Perform a click based on BFS for each round (BFS Implementation)
+    if (strtoupper($mode) == 'BFS') {
         for ($sum = 0; $sum <= $rows + $cols - 2; $sum++) {
             for ($i = 0; $i <= $sum; $i++) {
                 $j = $sum - $i;
                 if ($i < $rows && $j < $cols) {
-                    if ($matrix[$i][$j] === 'E'){
+                    if ($matrix[$i][$j] === 'E') {
                         explore($matrix, $i, $j);
                         $click = [$i, $j];
                         return [$matrix, $click];
@@ -62,15 +56,12 @@ function minesweeperRound($matrix){
             }
         }
         return [$matrix, $click];
-    }
-    else if(strtoupper($mode)=='DFS'){
+    } else if (strtoupper($mode) == 'DFS') {
         $click = [];
-        // Perform a click based on DFS for each round (DFS Implementation)
         foreach ($matrix as $x => $row) {
             foreach ($row as $y => $cell) {
                 if ($cell === 'E') {
                     explore($matrix, $x, $y);
-                
                     $click = [$x, $y];
                     return [$matrix, $click];
                 }
@@ -79,46 +70,49 @@ function minesweeperRound($matrix){
         return [$matrix, $click];
     }
 }
-  
 
-// Helper function to check if the game has finished
-function isGameFinished($matrix) {
+function isGameFinished($matrix)
+{
     foreach ($matrix as $row) {
         if (in_array('E', $row)) {
             return false;
         }
     }
     return true;
-  }
-
-//Implementation of game playing and validations
-function playMinesweeper($matrix){
-    $round = 1; // per te numeruar rundet 
-    $str = 'The sequence of clicks: ';
-
-    while (!isGameFinished($matrix)) { // kotrollo a eshte kryer loja
-        [$currentMat, $click] = minesweeperRound($matrix); // merr matricen e lojes dhe click matricen 
-
-        echo "Round $round:\n"; // printo matricen 
-        foreach ($currentMat as $row) {
-            foreach ($row as $cell) {
-                echo $cell . " ";
-            }
-            echo "\n";
-        }
-
-        echo "Click needed for this round: " . // ku eshte klikuar 
-            (empty($click) ? 'No more cells' : implode(",", $click)) . "\n\n";
-        $str.=implode(",", $click)."     ";
-
-        $matrix = $currentMat; // update matricen me vlerat e reja 
-        $round++; // inkremento sepse fillon rundi i ri 
-    }
-
-    echo "No more cells to explore. Game finished!"; // loja ka perfunduar
-    echo $str;
 }
 
+function solveMinesweeper($matrix)
+{
+    $steps = [];
+    while (!isGameFinished($matrix)) {
+        [$currentMat, $click] = minesweeperRound($matrix);
+        $matrix = $currentMat;
+        $steps[] = ['matrix' => $matrix, 'click' => $click];
+    }
+
+    return $steps;
+}
+
+function printMinesweeper($matrix, $row, $col, $click)
+{
+    echo '<table border="1">';
+    for ($i = 0; $i <= $row; $i++) {
+        echo '<tr>';
+        for ($j = 0; $j <= $col; $j++) {
+            $cell = $matrix[$i][$j];
+            if ($click && $i === $click[0] && $j === $click[1]) {
+                echo '<td >' . $cell . '</td>';
+            } else {
+                echo '<td>' . $cell . '</td>';
+            }
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+    if ($click) {
+        echo "<br>Clicked position: Row {$click[0]}, Column {$click[1]}<br><br>";
+    }
+}
 
 $mat =  [
     ['E', 'E', 'E', 'E', 'E','E', 'M', 'E', 'E'],
@@ -170,7 +164,17 @@ $mat =  [
 //     ['M', 'M', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E', 'E'],
 //     ['E', 'E', 'M', 'E', 'E', 'M', 'E', 'E', 'M', 'E', 'E', 'M', 'E', 'E', 'M', 'E', 'E', 'E', 'M', 'E', 'E', 'E', 'M', 'E', 'E', 'E', 'E', 'M', 'E', 'E'],
 // ];
+$solvedSteps = solveMinesweeper($mat);
 
-playMinesweeper($mat);
+echo "Initial Matrix:<br>";
+printMinesweeper($mat, count($mat) - 1, count($mat[0]) - 1, null);
 
+echo "<br><br>Solved Matrix:<br>";
+
+foreach ($solvedSteps as $step) {
+    $lastRow = count($step['matrix']) - 1;
+    $lastCol = count($step['matrix'][0]) - 1;
+    printMinesweeper($step['matrix'], $lastRow, $lastCol, $step['click']);
+    echo "<br><br>";
+}
 ?>
