1aa7e42c11ccde63a3b9ca279e95aba01a0c1f63
Lorena Mekaj <lorenamekaj@gmail.com>
2024-01-15 18:24:26 +0100

    feature/file organization for the js files
    Organized the files for the js part and also updated all the links.

--- minesweeper.js
+++ minesweeper.js
@@ -411,7 +411,7 @@ function submitAndFetch() {
 }
 
 function fetchGameSolution() {
-  return fetch("../solution.json")
+  return fetch("solution.json")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
